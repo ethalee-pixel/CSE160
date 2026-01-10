52e5dee37063f403211a70c484354f1b38b5d5f3
ethan893 <xethan101@gmail.com>
2026-01-09 17:00:47 -0800

    Test5

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -64,3 +64,62 @@ function handleDrawEvent()
     var v2 = new Vector3([x2,y2,0]);
     drawVector(v2,"blue");
 }
+function handleDrawOperation()
+{
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 400, 400);
+    var getx1 = document.getElementById('x1').value;
+    var gety1 = document.getElementById('y1').value;
+
+    var getOperation = document.getElementById('Operation-select');
+    var operation = getOperation.value;
+    var x1 = parseFloat(getx1)
+    var y1 = parseFloat(gety1)
+
+    var v1 = new Vector3([x1,y1,0]);
+    drawVector(v1,"red")
+
+    var getx2 = document.getElementById('x2').value;
+    var gety2 = document.getElementById('y2').value;
+    var getScalar = document.getElementById('scalar').value;
+    
+    var scalar = parseFloat(getScalar);
+    var x2 = parseFloat(getx2)
+    var y2 = parseFloat(gety2)
+
+    var v2 = new Vector3([x2,y2,0]);
+
+    if(operation == "add")
+    {
+        var additionVector = new Vector3(v1.elements);
+        additionVector.add(v2);
+        drawVector(additionVector,"green")
+    }
+    else if(operation == "sub")
+    {
+        var subtractionVector = new Vector3(v1.elements);
+        subtractionVector.sub(v2);
+        drawVector(subtractionVector,"green")
+    }
+        else if(operation == "div")
+    {
+        var divisionVector1 = new Vector3(v1.elements);
+        var divisionVector2 = new Vector3(v2.elements);
+        divisionVector1.div(scalar);
+        divisionVector2.div(scalar);
+        drawVector(divisionVector1,"green")
+        drawVector(divisionVector2,"green")
+    }
+            else if(operation == "mul")
+    {
+        var multiplicationVector1 = new Vector3(v1.elements);
+        var multiplicationVector2 = new Vector3(v2.elements);
+        multiplicationVector1.mul(scalar);
+        multiplicationVector2.mul(scalar);
+        drawVector(multiplicationVector1,"green")
+        drawVector(multiplicationVector2,"green")
+    }
+    drawVector(v2,"blue");
+}
